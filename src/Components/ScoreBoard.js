29d58df9e168f59e07e76a4a9ca20aee71f72e54
Nina Simmons <pkpbabies@gmail.com>
2025-02-12 12:01:50 -0500

    Add score board code

--- src/Components/ScoreBoard.js
+++ src/Components/ScoreBoard.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+const ScoreBoard = ({ score }) => (
+  <div className="score-board">
+    <div>Wins: {score.wins}</div>
+    <div>Losses: {score.losses}</div>
+    <div>Ties: {score.ties}</div>
+  </div>
+);
+
+export default ScoreBoard;
+
 
